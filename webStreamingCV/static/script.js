6191218de69b09d472ff7962d873496c25acd17e
GiorgeGi <97761991+GiorgeGi@users.noreply.github.com>
2023-11-04 14:32:52 +0200

    Update script.js

--- webStreamingCV/static/script.js
+++ webStreamingCV/static/script.js
@@ -6,3 +6,13 @@ socket.on('update_count', function(data) {
   var countElement = document.querySelector('.website-counter');
   countElement.textContent = data.count;
 });
+
+socket.on('connect', function() {
+  // Request the app uptime when connected to the server
+  socket.emit('get_app_uptime');
+});
+
+socket.on('update_app_uptime', function(uptime) {
+  var uptimeElement = document.getElementById('app-uptime');
+  uptimeElement.textContent = uptime;
+});
